59a903f72286a1d50e88c942d335334720effa0c
Anastas Kosstow <anastas.kosstow@protonmail.com>
2019-06-20 21:16:29 +0300

    Initial

---
+++ CSharp_Advanced/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CSharp_Advanced
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
